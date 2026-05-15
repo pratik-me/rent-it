3f0f61277ce6b50df59a27a8652c6358e9a39bcc
Pratik Kumar <pk.rpvv@gmail.com>
2026-05-15 19:46:39 +0530

    Added prisma

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Route Import
+
+// Configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Welcome to RENT IT")
+})
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
